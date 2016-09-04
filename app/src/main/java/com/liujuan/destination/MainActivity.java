7e6c0fd5e26d0571605dd03e150d4f78a48407f8
Juan Liu <janee.liujuan@gmail.com>
2016-09-04 18:40:05 +0200

    Added Google Location city name auto-complete

--- app/src/main/java/com/liujuan/destination/MainActivity.java
+++ app/src/main/java/com/liujuan/destination/MainActivity.java
@@ -4,16 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.liujuan.destination.model.City;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main";
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private ArrayList<City> mCities;
+    private PlaceAutocompleteFragment citySearchFragment;
+    private View citySearchCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +34,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mockCities();
         mRecyclerView = (RecyclerView) findViewById(R.id.destination_city_recyclerview);
+        citySearchCardView = findViewById(R.id.city_search_cardView);
 
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new GridLayoutManager(this, 2);
         mAdapter = new CityAdapter(mCities);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(mLayoutManager);
+
+        setupToolBar();
+        setupPlaceSelectListener();
+    }
+
+    private void setupToolBar() {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
     }
 
     private void mockCities() {
@@ -42,4 +63,59 @@ public class MainActivity extends AppCompatActivity {
         mCities.add(new City("Paris", "https://media.timeout.com/images/100644443/image.jpg"));
         mCities.add(new City("Barcelona", "https://media.timeout.com/images/100644443/image.jpg"));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.actionbar_search) {
+            showCitySearchFragment();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (citySearchCardView.getVisibility() == View.VISIBLE) {
+            hideCitySearchFragment();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void showCitySearchFragment() {
+        citySearchCardView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideCitySearchFragment() {
+        citySearchCardView.setVisibility(View.GONE);
+    }
+
+    private void setupPlaceSelectListener() {
+        citySearchFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        citySearchFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());
+                hideCitySearchFragment();
+            }
+
+            @Override
+            public void onError(Status status) {
+
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+    }
+
 }
