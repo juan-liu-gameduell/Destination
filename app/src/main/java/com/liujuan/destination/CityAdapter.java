1dfa09ad79601758058857e06cd668b3df48f8d5
Juan Liu <janee.liujuan@gmail.com>
2016-09-04 16:04:09 +0200

    add recyler view

---
+++ app/src/main/java/com/liujuan/destination/CityAdapter.java
@@ -0,0 +1,57 @@
+package com.liujuan.destination;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.liujuan.destination.model.City;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2016/9/4.
+ */
+public class CityAdapter extends RecyclerView.Adapter<CityAdapter.ViewHolder> {
+
+    private ArrayList<City> mCities;
+
+    public CityAdapter(ArrayList<City> cityiesData) {
+        mCities = cityiesData;
+    }
+
+    @Override
+    public CityAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.destination_city_recyclerview_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(CityAdapter.ViewHolder holder, int position) {
+        City city = mCities.get(position);
+        holder.cityName.setText(city.getName());
+        Context context = holder.cityImage.getContext();
+        Picasso.with(context).load(city.getImages().get(0)).resize(160,160).centerCrop().into(holder.cityImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCities.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView cityImage;
+        private TextView cityName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            cityImage = (ImageView) itemView.findViewById(R.id.destination_city_image);
+            cityName = (TextView) itemView.findViewById(R.id.destination_city_name);
+        }
+    }
+}
--- app/src/main/java/com/liujuan/destination/MainActivity.java
+++ app/src/main/java/com/liujuan/destination/MainActivity.java
@@ -2,12 +2,44 @@ package com.liujuan.destination;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.liujuan.destination.model.City;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<City> mCities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mockCities();
+        mRecyclerView = (RecyclerView) findViewById(R.id.destination_city_recyclerview);
+
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new GridLayoutManager(this, 2);
+        mAdapter = new CityAdapter(mCities);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+    }
+
+    private void mockCities() {
+        mCities = new ArrayList<>();
+        mCities.add(new City("Berlin", "http://www.planetware.com/photos-large/D/east-berlin-former-0.jpg"));
+        mCities.add(new City("Hamburger", "http://www.eia-jc.org/fileadmin/BILDER/HAMBURG/Hamburger_Rathaus_Luftperspektive.jpg"));
+        mCities.add(new City("Beijing", "http://www.telegraph.co.uk/content/dam/Travel/Destinations/Asia/China/Beijing/Beijing-cityguide-statue-xlarge.jpg"));
+        mCities.add(new City("London", "https://media.timeout.com/images/100644443/image.jpg"));
+        mCities.add(new City("Tokyo", "https://www.burgessyachts.com/media/adminforms/locations/t/o/tokyo_1_1.jpg"));
+        mCities.add(new City("New York", "https://www.omnihotels.com/-/media/images/hotels/nycber/destinations/nyc-aerial-skyline.jpg?h=660&la=en&w=1170"));
+        mCities.add(new City("Sydney", "https://media.timeout.com/images/100644443/image.jpg"));
+        mCities.add(new City("Washington", "https://media.timeout.com/images/100644443/image.jpg"));
+        mCities.add(new City("Paris", "https://media.timeout.com/images/100644443/image.jpg"));
+        mCities.add(new City("Barcelona", "https://media.timeout.com/images/100644443/image.jpg"));
     }
 }
